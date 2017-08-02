b5a73191faa8ae8cde0384ebef3391e9d4a25086
PixelSoftwareStudios <30636538+PixelSoftwareStudios@users.noreply.github.com>
2017-08-02 07:49:03 -0400

    Add files via upload

--- autoclicker.js
+++ autoclicker.js
@@ -2,19 +2,18 @@ const robot = require("robotjs");
 
 let on = false;
 let interval;
+let speed = 1200;
 
 exports.toggle = function() {
 	if (on) {
 		clearInterval(interval);
 		on = false;
-		exports.interval = interval;
 	} else if (!on) {
 		on = true;
 		interval = setInterval(function() {
 			if (on) {
 				robot.mouseClick("left");
 			}
-		}, 1200);
-		exports.interval = interval;
+		}, speed);
 	}
 }
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 'use strict';
 const electron = require('electron');
-const autoClicker = require("./autoclicker.js");
 const globalShortcut = require("electron").globalShortcut;
+const autoClicker = require("./autoclicker.js");
 
 const app = electron.app;
-// adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
 
-// prevent window being garbage collected
 let mainWindow;
 
 function onClosed() {
-	// dereference the window
-	// for multiple windows store them in an array
 	mainWindow = null;
 }
 
@@ -23,7 +18,6 @@ function createMainWindow() {
 		title: "Autoclicker by Pixel Studios"
 	});
 
-	win.loadURL(`file://${__dirname}/index.html`);
 	win.on('closed', onClosed);
 
 	return win;
@@ -31,7 +25,6 @@ function createMainWindow() {
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
-		clearInterval(autoClicker.interval);
 		app.quit();
 	}
 });
@@ -47,6 +40,5 @@ app.on('ready', () => {
 		autoClicker.toggle();
 	});
 
-	console.log("hello");
 	mainWindow = createMainWindow();
 });
